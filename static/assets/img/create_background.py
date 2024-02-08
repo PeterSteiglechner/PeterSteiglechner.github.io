7d4abdff5cfce69f9b5fb45b34bfc757284b2b1d
PeterSteiglechner <peter.steiglechner@gmail.com>
2024-02-08 21:25:48 +0100

    added pictures, changed map

--- static/assets/img/create_background.py
+++ static/assets/img/create_background.py
@@ -2,11 +2,9 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
-plt.get_cmap("coolwarm")
-X, Y = np.meshgrid(np.arange(0,1920), np.arange(0, 400))
-fig, ax = plt.subplots(1,1, figsize=(19.2, 4))
-plt.pcolormesh(X, Y, X, cmap="coolwarm", vmin = 0,vmax=1920,)
+X, Y = np.meshgrid(np.arange(0,1920), np.arange(0, 100))
+fig, ax = plt.subplots(1,1, figsize=(19.2, 1))
+plt.pcolormesh(X, Y, X, cmap="summer", vmin = 0,vmax=1920,)
 fig.subplots_adjust(left=0, top=1, bottom=0, right=1)
-print(X[:, 0])
 fig.set_facecolor("y")
-plt.savefig("background.jpeg")
+plt.savefig("background_summer.jpeg")
