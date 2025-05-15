f343b4da40076c352dd57cdb3f5df548ec1dfaba
PeterSteiglechner <peter.steiglechner@gmail.com>
2025-05-16 00:41:21 +0200

    updated backgroud

--- static/assets/img/create_background.py
+++ static/assets/img/create_background.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
-X, Y = np.meshgrid(np.arange(0,1920), np.arange(0, 100))
+X, Y = np.meshgrid(np.arange(0,1920), np.arange(0, 20))
 fig, ax = plt.subplots(1,1, figsize=(19.2, 1))
-plt.pcolormesh(X, Y, X, cmap="summer", vmin = 0,vmax=1920,)
+plt.pcolormesh(X, Y, X, cmap="Spectral", vmin = 0,vmax=1920,)
 fig.subplots_adjust(left=0, top=1, bottom=0, right=1)
 fig.set_facecolor("y")
-plt.savefig("background_summer.jpeg")
+plt.savefig("background.jpeg")
